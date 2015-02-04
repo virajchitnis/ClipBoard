73dc8adfdfe5951794c0870cef07db6bf5bec7d2
Viraj Chitnis <chitnisviraj@gmail.com>
2015-02-04 12:22:23 -0500

    Clip board design complete
    The design for the clipboard is complete.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET clipboard page. */
+router.get('/boardid', function(req, res, next) {
+  res.render('clipboard', { title: 'Express' });
+});
+
 module.exports = router;
